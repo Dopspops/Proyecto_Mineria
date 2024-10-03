7d57ece9951abf754b213fe67a4233edb8aaffb3
ferdinand bierbaum <ferdibierbaum@gmail.com>
2024-10-02 21:26:48 -0600

    Changes 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ from skimage import io
 #----- Lectura de la Imagen ---------------------------------------
 Logo = io.imread(r"./Imagenes/ITESO_Logo.png")
 
+
 #----- Renderizado de la Imagen -----------------------------------
 st.image(Logo, width = 500)
 
@@ -31,6 +32,12 @@ st.image(Logo, width = 500)
 st.title("Proyecto de Programación para Mineria de Datos")
 st.subheader(":blue[Ferdinand Josef Bierbaum Agular - Exp. 739162]")
 st.markdown("Este proyecto se estaría basando sobre los datos proporcionados de MiBici. Se tienen registros desde 2015 al 2024.")
+st.divider()
+
+
+# ------- Grafico General todos los Años (Box) --------------------
+st.subheader("Gráfico de Caja - Todos los Años (en base al Año de Nacimiento)")
+
 
 
 #------------------------------------------------------------------
@@ -40,5 +47,11 @@ st.markdown("Este proyecto se estaría basando sobre los datos proporcionados de
 #----- Renderizado de la Imagen y el Título en el Dashboard -------
 st.sidebar.image(Logo, width = 200)
 st.sidebar.markdown("## MENÚ DE CONFIGURACIÓN")
+st.sidebar.divider()
+
+#------ Selector sobre año para graficos -------------------------
+vars_anios = ['2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']
+anio_default = vars_anios.index('2024')
+anio_most_graficos = st.sidebar.selectbox('Selecciona el Año para Mostrar los Gráficos:', vars_anios, index = anio_default)
 st.sidebar.divider()
 
